77142cb13021b413fbf9ad49aee8e30e8ed9bb2c
gaowanlu <2209120827@qq.com>
2025-12-06 10:26:02 +0000

    feat: Map FixedUpdate

--- MapSvr/lua/Map/MapLogic.lua
+++ MapSvr/lua/Map/MapLogic.lua
@@ -14,9 +14,12 @@ function Map.new(mapId)
 
     -- 帧率
     self.MapDbData.TICK_RATE = 20
-    -- 每帧时间间隔
+    -- 每帧时间间隔 毫秒
     self.MapDbData.DT_MS = 1000 // self.MapDbData.TICK_RATE
+    -- 执行上一次tick的毫秒时间戳
     self.MapDbData.lastTickTimeMS = 0
+    -- 帧时常累计时间毫秒
+    self.MapDbData.durationAccumulator = 0
 
     return self
 end
@@ -27,10 +30,23 @@ end
 
 function Map:OnTick()
     local timeMS = TimeMgr.GetMS()
-    if (timeMS - self.MapDbData.lastTickTimeMS) >= self.MapDbData.DT_MS then
-        self.MapDbData.lastTickTimeMS = timeMS
-        -- Log:Error("MapId %d MS %d", self.MapDbData.id, timeMS)
+
+    local frameTime = timeMS - self.MapDbData.lastTickTimeMS
+    if frameTime > 250 then
+        frameTime = 250 -- 防止卡顿时爆炸
     end
+    self.MapDbData.lastTickTimeMS = timeMS
+    self.MapDbData.durationAccumulator = self.MapDbData.durationAccumulator + frameTime
+
+    while self.MapDbData.durationAccumulator >= self.MapDbData.DT_MS do
+        -- 一次固定步长的逻辑更新
+        self:FixedUpdate()
+        self.MapDbData.durationAccumulator = self.MapDbData.durationAccumulator - self.MapDbData.DT_MS
+    end
+end
+
+function Map:FixedUpdate()
+    -- Log:Error("MapId %d FixedUpdate", self.MapDbData.id)
 end
 
 return Map;
