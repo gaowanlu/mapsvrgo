f4e946788946cc28b8d81cd58c4c4958c7e7da94
gaowanlu <2209120827@qq.com>
2026-01-01 12:53:14 +0000

    update: better Init.lua

--- lua/Init.lua
+++ lua/Init.lua
@@ -11,60 +11,70 @@ package.path = avant.LuaDir .. "/ProtoLua/?.lua;" .. package.path
 
 local avant = require("Avant")
 local Log = require("Log")
-local Main = require("Main")
-local Worker = require("Worker")
-local Other = require("Other")
 
 function OnMainInit()
+    local Main = require("Main")
     Main:OnInit();
 end
 
 function OnMainStop()
+    local Main = require("Main")
     Main:OnStop();
 end
 
 function OnMainTick()
+    local Main = require("Main")
     Main:OnTick();
 end
 
 function OnMainReload()
+    local Main = require("Main")
     Main:OnReload();
 end
 
 function OnWorkerInit(workerIdx)
+    local Worker = require("Worker")
     Worker:OnInit(workerIdx);
 end
 
 function OnWorkerStop(workerIdx)
+    local Worker = require("Worker")
     Worker:OnStop(workerIdx);
 end
 
 function OnWorkerTick(workerIdx)
+    local Worker = require("Worker")
     Worker:OnTick(workerIdx);
 end
 
 function OnWorkerReload(workerIdx)
+    local Worker = require("Worker")
     Worker:OnReload(workerIdx);
 end
 
 function OnOtherInit()
+    local Other = require("Other")
     Other:OnInit();
 end
 
 function OnOtherStop()
+    local Other = require("Other")
     Other:OnStop();
 end
 
 function OnOtherTick()
+    local Other = require("Other")
     Other:OnTick();
 end
 
 function OnOtherReload()
+    local Other = require("Other")
     Other:OnReload();
 end
 
 function OnLuaVMRecvMessage(isMainVM, isOtherVM, isWorkerVM, workerIdx, msg_type, cmd, message, uint64_param1,
                             int64_param2, str_param3)
+    local Other = require("Other")
     if isOtherVM then
         Other:OnLuaVMRecvMessage(msg_type, cmd, message, uint64_param1, int64_param2, str_param3);
     end
